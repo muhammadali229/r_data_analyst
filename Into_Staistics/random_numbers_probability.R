e4257005870f04e6ae71c783971e86f93af35507
muhammadali229 <learningacademy229@gmail.com>
2021-08-08 03:54:47 +0500

    Discrete Distribution

--- Into_Staistics/random_numbers_probability.R
+++ Into_Staistics/random_numbers_probability.R
@@ -20,3 +20,42 @@ amir_deals %>%
 amir_deals %>%
   sample_n(5, replace = TRUE)
 ###############################################################
+restaurant_groups <- data.frame(
+  group_id = c("A", "B", "C", "D", "E", "F", "G", "H", "I", "J"),
+  group_size = c(2, 4, 6, 2, 2, 2, 3, 2, 4, 2)
+)
+# Create a histogram of group_size
+ggplot(restaurant_groups, aes(group_size)) +
+  geom_histogram(bins = 5)
+# Create probability distribution
+size_distribution <- restaurant_groups %>%
+  # Count number of each group size
+  count(group_size) %>%
+  # Calculate probability
+  mutate(probability = n / sum(n))
+size_distribution
+# Calculate expected group size
+expected_val <- sum(size_distribution$probability *
+                      size_distribution$group_size)
+expected_val
+# Create probability distribution
+size_distribution <- restaurant_groups %>%
+  count(group_size) %>%
+  mutate(probability = n / sum(n))
+# Calculate probability of picking group of 4 or more
+size_distribution %>%
+  # Filter for groups of 4 or larger
+  filter(group_size >= 4) %>%
+  # Calculate prob_4_or_more by taking sum of probabilities
+  summarize(prob_4_or_more = sum(probability))
+###############################################################
+
+
+
+
+
+
+
+
+
+###############################################################
